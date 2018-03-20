432cee47bd3a5d15dfa77cd2f2d5deff2b78575a
Nat <natwales@gmail.com>
2018-03-20 10:46:12 -0400

    Updated jointExporter.py to extract root joints to world space with _ROOT labels

--- jointExporter.py
+++ jointExporter.py
@@ -1,8 +1,11 @@
 from maya import cmds
+import maya.OpenMaya as om
+import math
 import json
 import os
 
 def saveSelectedJointsToJsonFile(startTime, endTime, filename = "joints"):
+
      objects = cmds.ls(selection=True, type="joint", dag=True, long=True)
      joints = {}
      timeRange = range(startTime,endTime+1,1)
@@ -11,30 +14,64 @@ def saveSelectedJointsToJsonFile(startTime, endTime, filename = "joints"):
 
          jointDict = {}
          shortName = obj.split("|")[-1]
-         #attrs = cmds.listAttr(r=True)
-         #print attrs
+         isRoot = False
+         if "_ROOT_" in shortName:
+             isRoot = True
+
+         print cmds.listAttr(r=True)
 
          for frame in timeRange:
-             tx, ty, tz = cmds.getAttr('%s.translate' % obj, time =frame)[0]
-             rx, ry, rz  = cmds.getAttr('%s.rotate' % obj, time=frame)[0]
-             ox, oy, oz = cmds.getAttr('%s.jointOrient' % obj, time=frame)[0]
-
-             frameDict = {}
-
-             frameDict['tx'] = "%.8f" % tx
-             frameDict['ty'] = "%.8f" % ty
-             frameDict['tz'] = "%.8f" % tz
-             frameDict['rx'] = "%.8f" % (rx + ox)
-             frameDict['ry'] = "%.8f" % (ry + oy)
-             frameDict['rz'] = "%.8f" % (rz + oz)
-
-             jointDict[frame] = frameDict
+             if isRoot:
+                jointDict[frame] = _getFrameDictForRootJoint(obj,frame)
+             else:
+                jointDict[frame] = _getFrameDict(obj,frame)
 
          joints[shortName] = jointDict
 
      jsonObject = json.dumps(joints)
      _saveJson(jsonObject, filename)
 
+#non root joints should pass values from object space
+def _getFrameDict(obj, frame):
+
+    tx, ty, tz = cmds.getAttr('%s.translate' % obj, time =frame)[0]
+    rx, ry, rz  = cmds.getAttr('%s.rotate' % obj, time=frame)[0]
+    ox, oy, oz = cmds.getAttr('%s.jointOrient' % obj, time=frame)[0]
+
+    frameDict = {}
+
+    frameDict['tx'] = "%.8f" % tx
+    frameDict['ty'] = "%.8f" % ty
+    frameDict['tz'] = "%.8f" % tz
+    frameDict['rx'] = "%.8f" % (rx + ox)
+    frameDict['ry'] = "%.8f" % (ry + oy)
+    frameDict['rz'] = "%.8f" % (rz + oz)
+    return frameDict
+
+#root joints pass values from world space
+def _getFrameDictForRootJoint(obj,frame):
+
+    #get world matrix for root joint
+    rootJointWMat = om.MMatrix()
+    om.MScriptUtil.createMatrixFromList(cmds.getAttr('%s.worldMatrix'%obj), rootJointWMat)
+    jointWorldMat = om.MTransformationMatrix(rootJointWMat)
+
+    #read rotation in world space
+    rotOrder = cmds.getAttr('%s.rotateOrder'%obj)
+    eulerRot = jointWorldMat.eulerRotation()
+    eulerRot.reorderIt(rotOrder)
+    translation = jointWorldMat.getTranslation(om.MSpace.kWorld)
+    angles = [math.degrees(angle) for angle in (eulerRot.x, eulerRot.y, eulerRot.z)]
+
+    frameDict = {}
+    frameDict['tx'] = "%.8f" % translation.x
+    frameDict['ty'] = "%.8f" % translation.y
+    frameDict['tz'] = "%.8f" % translation.z
+    frameDict['rx'] = "%.8f" % (angles[0])
+    frameDict['ry'] = "%.8f" % (angles[1])
+    frameDict['rz'] = "%.8f" % (angles[2])
+
+    return frameDict
 
 def _saveJson(jsonObject, filename):
     if ".json" not in filename:
